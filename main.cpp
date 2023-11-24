d7a93ac55efe69b7c4496dbe197ed21297a6dacd
Toni Borrull <toni.borrull@enti.cat>
2023-11-24 10:57:10 +0100

    Cambio borrando comentario

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 
 int main (int argc, char** argv){
 
-	//std::cout << argc << std::endl;
 	
 	if(argc > 1){
 		
-	std::cout << "Holi"<< argv[1] << "!" << std::endl;
+	std::cout << "Holi "<< argv[1] << "!" << std::endl;
 	}else{
 	
 		std::cout <<"Introduce un nombre como parametro!!!" << std::endl;
